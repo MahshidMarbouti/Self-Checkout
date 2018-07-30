e82d381c77a082bad9654e24c5c08066a7447dee
Mahshid Marbouti <mahshid.marbouti@gmail.com>
2018-07-30 13:08:42 -0600

    create solution.

---
+++ Self-Checkout/Self-Checkout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Self_Checkout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
