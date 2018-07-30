b746c67305088694c936bde840680d3f57312cb6
Mahshid Marbouti <mahshid.marbouti@gmail.com>
2018-07-30 15:48:36 -0600

    ShoppingCart.

---
+++ Self-Checkout/Self-Checkout/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic; 
+namespace Self_Checkout
+{
+    public class ShoppingCart
+    {
+        public int ID;
+        public int Capacity;
+        public List<Shopping_Cart_Item> items;
+        public ShoppingCart(int id, int capacity)
+        {
+            ID = id;
+            Capacity = capacity;
+            items = new List<Shopping_Cart_Item>();
+        }
+        public List<Shopping_Cart_Item> add(Shopping_Cart_Item item)
+        {
+            items.Add(item);
+            return items;
+        }
+
+        public List<Shopping_Cart_Item> remove(Shopping_Cart_Item item)
+        {
+            items.Remove(item);
+            return items;
+        }
+    }
+}
