f96a6ebdef8fe71db4d7905907be0ae17f25ab93
Mahshid Marbouti <mahshid.marbouti@gmail.com>
2018-07-30 15:55:28 -0600

    checkout

---
+++ Self-Checkout/Self-Checkout/Checkout.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Self_Checkout
+{
+    public class Checkout
+    {
+        ShoppingCart Shoppingcart;
+        DateTime Date;
+        int ID;
+        public Checkout(ShoppingCart shopping, DateTime date)
+        {
+            Shoppingcart = shopping;
+            Date = date;
+        }
+        public int Generante_Reciept()
+        {
+            Recipet receipt = new Recipet(ID, Shoppingcart);
+            int price = receipt.Print_Reciept();
+            return price;
+
+        }
+        public bool processPayment(PaymentType type, float amount)
+        {
+            Payment payment = new Payment(type, amount);
+            return payment.processPayment();
+                          
+
+        }
+    }
+}
