84c55b2909f899ee80679804d2987ad16a075318
Mahshid Marbouti <mahshid.marbouti@gmail.com>
2018-07-30 15:55:14 -0600

    Add payment.

---
+++ Self-Checkout/Self-Checkout/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Self_Checkout
+{
+    public class Payment
+    {
+        public PaymentType Type;
+        public float Amount{ get; set; }
+        public Payment(PaymentType type, float amount){
+            Amount = amount;
+            Type = type;
+            
+        }
+        public bool processPayment()
+        {
+
+            return true; 
+        }
+    }
+}
---
+++ Self-Checkout/Self-Checkout/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Self_Checkout
+{
+    public class Product
+    {
+        public int ID;
+        public ProductType type;
+        public int price;
+        public string Name;
+    }
+}
