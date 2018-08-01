458d8ae8bf2b7ac7c9775d596a8342ee02bc8ee1
Mahshid Marbouti <mahshid.marbouti@gmail.com>
2018-07-31 19:53:29 -0600

    Check for invalid conditions in the line.

--- Self-Checkout/Self-Checkout/Product.cs
+++ Self-Checkout/Self-Checkout/Product.cs
@@ -7,7 +7,7 @@
         public int Price{ get; private set; }
         public string Name{ get; private set; }
         public int Id { get; private set; }
-      
+
         private Product(int id, ProductType type, int price, string name){
             Id = id;
             Type = type;
@@ -16,7 +16,17 @@
         }
 
         public static Product CreateFromLine(string line){
+            const int LineSize = 4;
+                
             var items = line.Split(',');
+
+            if (items.Length!=LineSize){
+                throw new System.InvalidOperationException($"Each line should have {LineSize} items separated by comma.");
+            }
+            int tempId;
+            if (!int.TryParse(items[0], out tempId)){
+                throw new System.InvalidCastException($"Id must be an integet value, current value is {items[0]}");
+            }
             return new Product(int.Parse(items[0]), new ProductType(items[1]), int.Parse(items[2]), items[3]);
         }
     }
